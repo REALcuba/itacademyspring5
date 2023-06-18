328842c9146a3ad853570f2003c87772060c45bc
REALcuba <angelskcuba@gmail.com>
2023-06-18 12:27:02 +0200

    fetch and paint joke

--- index.ts
+++ index.ts
@@ -3,12 +3,24 @@
 const container = document.getElementById('container')
 
 const jokesContainer = document.createElement('div')
-jokesContainer.className = 'container bg-light'
+jokesContainer.className = 'card bg-light'
 jokesContainer.innerHTML = '<h1 class="text-center">Jokes</h1>'
 
 container?.appendChild(jokesContainer)
 
 
-// const fetchJokes = async () => {
+const fetchJokes = async () => {
+ await fetch('https://icanhazdadjoke.com/',{
+    headers: {
+        'Accept': 'application/json'
+    }})
+.then(response => response.json())
+.then(data => {
+jokesContainer.innerHTML = `<p class="text-center card-body pb-0">${data.joke}</p>`
 
-// }
+}
+)}
+
+const btn =document.getElementById('button')
+btn?.addEventListener('click', fetchJokes)
+
