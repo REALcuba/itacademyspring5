9377282169051ab1dee527e589c174c100296645
REALcuba <angelskcuba@gmail.com>
2023-06-18 11:08:55 +0200

    add ts and first template

---
+++ index.js
@@ -0,0 +1,8 @@
+// const app = document.getElementById('app')
+var container = document.getElementById('container');
+var jokesContainer = document.createElement('div');
+jokesContainer.className = 'container bg-light';
+jokesContainer.innerHTML = '<h1 class="text-center">Jokes</h1>';
+container === null || container === void 0 ? void 0 : container.appendChild(jokesContainer);
+// const fetchJokes = async () => {
+// }
---
+++ index.ts
@@ -0,0 +1,14 @@
+// const app = document.getElementById('app')
+
+const container = document.getElementById('container')
+
+const jokesContainer = document.createElement('div')
+jokesContainer.className = 'container bg-light'
+jokesContainer.innerHTML = '<h1 class="text-center">Jokes</h1>'
+
+container?.appendChild(jokesContainer)
+
+
+// const fetchJokes = async () => {
+
+// }
